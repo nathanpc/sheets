c7b2fb87572ecc2105f3d81b7042444ad2b54bf2
Nathan Campos <eeepc904@gmail.com>
2013-05-11 18:30:34 -0300

    Parsing the arguments

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,29 @@
  */
 
 #include <iostream>
-#include <vector> // REMOVE
-#include <string> // REMOVE
+#include <vector>
+#include <string>
+#include <cstring>
+#include <boost/filesystem.hpp>
 
 #include "dir_parse.h"
 #include "db.h"
 #include "color.h"
+
 using namespace std;
+namespace fs = boost::filesystem;
 
-int main(int argc, char *argv[]) {
-	// TODO: Parse command-line stuff.
-	// TODO: Store the database inside the config dir at ~
+void usage() {
+	cout << BOLDWHITE << "Usage: " << RESET << "sheets [-i|-s] directory|term" << endl << endl;
 
+	cout << BOLDWHITE << "Commands:" << RESET << endl;
+	cout << "    -i(index)\t  Indexes a directory" << endl;
+	cout << "    -s(search)\t  Search for a datasheet using a term" << endl;
+}
+
+void index(const char *directory) {
 	// Setup the directories stuff.
-	Dir_Parse dir(argv[1]);
+	Dir_Parse dir(directory);
 	dir.iterate();
 
 	// Setup the database stuff.
@@ -38,6 +47,33 @@ int main(int argc, char *argv[]) {
 	}
 
 	cout << endl << BOLDBLUE << "Successfully indexed " << dir.datasheets.size() << " datasheets." << RESET << endl;
+}
+
+int main(int argc, char *argv[]) {
+	// TODO: Store the database inside the config dir at ~
+
+	// Check if there's at least one argument.
+	if (argc == 1) {
+		usage();
+		return 1;
+	}
+
+	// Check if the user just typed the directory with no commands.
+	if (argv[1][0] != '-' && strcmp(argv[1], "index") != 0 && strcmp(argv[1], "search") != 0) {
+		if (fs::is_directory(argv[1])) {
+			index(argv[1]);
+		}
+	}
+
+	// The usual argument parsing stuff.
+	if (strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "index") == 0) {
+		index(argv[1]);
+	} else if (strcmp(argv[1], "-s") == 0 || strcmp(argv[1], "search") == 0) {
+		cout << "Search for: " << argv[1] << endl;
+	} else {
+		usage();
+		return 1;
+	}
 
 	return 0;
 }
